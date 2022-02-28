8adfb08bfa71a28d439c1b592e0b5fed06bc3d9f
Alex Pozas-Kerstjens <apozas@users.noreply.github.com>
2022-02-28 09:27:52 +0100

    More tabulation fixes

--- database_processing.py
+++ database_processing.py
@@ -18,20 +18,21 @@ import pandas as pd
 # Load data
 data_path = 'globaldothealth_2021-03-22.csv'
 data  	  = pd.read_csv(data_path,
-		        usecols=['location.country',
-				 'events.outcome.value',
-				 'events.confirmed.date',
-				 'demographics.ageRange.start',
-				 'demographics.gender',
-				 'symptoms.status'])
+				        usecols=['location.country',
+								 'events.outcome.value',
+								 'events.confirmed.date',
+								 'demographics.ageRange.start',
+								 'demographics.gender',
+								 'symptoms.status'])
 
 # Simplify column names
 data.rename(columns={'events.confirmed.date': 'date',
-		     'location.country': 'country',
-		     'demographics.ageRange.start': 'age',
-		     'demographics.gender': 'gender',
-		     'symptoms.status': 'symptomatic',
-		     'events.outcome.value': 'outcome'}, inplace=True)
+				     'location.country': 'country',
+				     'demographics.ageRange.start': 'age',
+				     'demographics.gender': 'gender',
+				     'symptoms.status': 'symptomatic',
+				     'events.outcome.value': 'outcome'},
+			inplace=True)
 
 # Select countries of interest
 data_Argentina = data[data['country'] == 'Argentina']
@@ -42,21 +43,23 @@ data = pd.concat([data_Argentina, data_Colombia])
 # Clean the outcome column. All admissions to a hospital (either in a standard
 # bed or in ICU) are treated equally
 data['outcome'].replace({'icuAdmission': 'hospitalAdmission',
-			 'Unknown': float('nan'),
-			 '\)': ''}, regex=True, inplace=True)
+						 'Unknown': float('nan'),
+						 '\)': ''},
+						regex=True,
+						inplace=True)
 data.dropna(inplace=True)
 
 # Balance points according to country
 points_col, points_arg = data['country'].value_counts()
-step 		       = points_col // points_arg
+step 		       	   = points_col // points_arg
 
 colombia_mask     = (data['country'] == 'Colombia')
 nonrecovered_mask = (data['outcome'] == 'Death')
 data_cn           = data[colombia_mask    & nonrecovered_mask][::step]
-data_cr 	  = data[colombia_mask    & (~nonrecovered_mask)][::step]
-data_an 	  = data[(~colombia_mask) & nonrecovered_mask]
-data_ar 	  = data[(~colombia_mask) & (~nonrecovered_mask)]
-data 		  = pd.concat([data_cn, data_cr, data_an, data_ar])
+data_cr 	  	  = data[colombia_mask    & (~nonrecovered_mask)][::step]
+data_an 	  	  = data[(~colombia_mask) & nonrecovered_mask]
+data_ar 	  	  = data[(~colombia_mask) & (~nonrecovered_mask)]
+data 		  	  = pd.concat([data_cn, data_cr, data_an, data_ar])
 
 # Use boolean variable for symptoms
 data['symptomatic'] = (data['symptomatic'] == 'Symptomatic')
@@ -82,14 +85,14 @@ recovered    = data[data.outcome == 'Recovered']
 nonrecovered = data[data.outcome == 'Death']
 
 # There are more recovered than not, so the latter gives us the dataset size
-count		 = len(nonrecovered) // 4
+count		 	 = len(nonrecovered) // 4
 recovered_arg    = recovered[recovered.country == 'Argentina']
 recovered_col    = recovered[recovered.country == 'Colombia']
 balanced_dataset = pd.concat([nonrecovered,
-			      recovered_arg[recovered_arg.odd].iloc[:count],
-			      recovered_col[recovered_col.odd].iloc[:count],
-			      recovered_arg[~recovered_arg.odd].iloc[:count],
-			      recovered_col[~recovered_col.odd].iloc[:count]])
+						      recovered_arg[recovered_arg.odd].iloc[:count],
+						      recovered_col[recovered_col.odd].iloc[:count],
+						      recovered_arg[~recovered_arg.odd].iloc[:count],
+						      recovered_col[~recovered_col.odd].iloc[:count]])
 balanced_dataset.sort_index(inplace=True)
 
 ################################################################################
@@ -97,15 +100,15 @@ balanced_dataset.sort_index(inplace=True)
 ################################################################################
 # Replace strings for booleans
 balanced_dataset.replace({'country': {'Argentina': True, 'Colombia': False},
-		          'gender':  {'Female': True, 'Male': False},
-			  'outcome': {'Death': False, 'Recovered': True}},
-		         inplace=True)
+				          'gender':  {'Female': True, 'Male': False},
+					  	  'outcome': {'Death': False, 'Recovered': True}},
+		         		 inplace=True)
 
 # More descriptive column names
 balanced_dataset.rename(columns={'country': 'argentina',
-				 'gender':  'female',
-				 'outcome': 'recovered'},
-			inplace=True)
+								 'gender':  'female',
+								 'outcome': 'recovered'},
+						inplace=True)
 
 # Final export
 if not os.path.isdir('datasets'):
